07a0056512e8007d9ea28f6ef4d4fe4754ca2a5b
AliSiddique <alisiddique10@hotmail.com>
2025-01-13 17:22:30 +0000

    Initial Commit

---
+++ transcript-ai-meeting/transcript_ai_meetingApp.swift
@@ -0,0 +1,32 @@
+//
+//  transcript_ai_meetingApp.swift
+//  transcript-ai-meeting
+//
+//  Created by Ali Siddique on 1/13/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct transcript_ai_meetingApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
